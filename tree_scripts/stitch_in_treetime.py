2f77d4befc97a436e10e70a5f97386e116e3c291
Sanket Wagle <96889743+sanketwagle@users.noreply.github.com>
2026-04-29 16:05:24 -0500

    Added aggresive filtering to stitch in treetime

--- tree_scripts/stitch_in_treetime.py
+++ tree_scripts/stitch_in_treetime.py
@@ -5,9 +5,13 @@ parser = argparse.ArgumentParser(prog="stitch_in_treetime",description="removes 
 parser.add_argument("input_treefile")
 parser.add_argument("output_path")
 parser.add_argument("output_schema")
+parser.add_argument("--aggressive_filtering",action="store_true")
 
 args = parser.parse_args()
 
 t = dendropy.Tree.get(path=args.input_treefile,schema="nexus")
-t.write(path=args.output_path,schema=args.output_schema)
+if args.aggressive_filtering:
+    t.write(path=args.output_path,schema=args.output_schema,suppress_internal_node_labels=True,suppress_internal_taxon_labels=True)
+else:
+    t.write(path=args.output_path,schema=args.output_schema)
 
