851363e152b70755f083104de38f116ea1fdd3d8
Sanket Wagle <96889743+sanketwagle@users.noreply.github.com>
2026-04-29 16:02:09 -0500

    Added smot reverse grep

---
+++ misc/smot_reverse_grep.sh
@@ -0,0 +1,8 @@
+# Hack for handling smot reverse grep using a file as an input 
+# Should probably be deleted once this is implemented in upstream
+
+# input 1: treefile path
+# input 2: file containing taxa that need to be excluded
+
+excluded_taxa=$(join_by "|" $(cat $1))
+smof grep -vP "($excluded_taxa)"
