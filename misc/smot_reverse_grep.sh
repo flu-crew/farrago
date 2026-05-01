da72af0384d547807567769d8159c00cea043855
Sanket Wagle <96889743+sanketwagle@users.noreply.github.com>
2026-05-01 13:17:21 -0500

    Forgot to add join_by in previous smot_reverse_grep

--- misc/smot_reverse_grep.sh
+++ misc/smot_reverse_grep.sh
@@ -4,5 +4,12 @@
 # input 1: treefile path
 # input 2: file containing taxa that need to be excluded
 
+
+function join_by {
+    local IFS="$1";
+    shift;
+    echo "$*"
+}
+
 excluded_taxa=$(join_by "|" $(cat $1))
 smof grep -vP "($excluded_taxa)"
