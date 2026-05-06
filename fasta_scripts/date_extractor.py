a3181ea9dbda9ea4cec37375d6eb8b11a0ea3c27
Sanket Wagle <96889743+sanketwagle@users.noreply.github.com>
2026-05-06 14:53:10 -0500

    Added option to specify date column

--- fasta_scripts/date_extractor.py
+++ fasta_scripts/date_extractor.py
@@ -5,6 +5,7 @@ from datetime import datetime
 parser = argparse.ArgumentParser(prog="date_extractor",description="Extracts dates for treetime given a fasta file, assuming that dates are the last column")
 parser.add_argument("filename")
 parser.add_argument("output_file_name")
+parser.add_argument("--date_column",default=-1)
 
 args = parser.parse_args()
 
@@ -14,7 +15,7 @@ with open(args.output_file_name,"w+") as date_file:
     date_file.write("name,date,dec_date\n")
     for record in records:
         name = record.name
-        date_str = name.split("|")[-1]
+        date_str = name.split("|")[args.date_column]
         date = datetime.timetuple(datetime.strptime(date_str,"%Y-%m-%d"))
         dec_date = date.tm_year + (date.tm_yday/365)
         date_file.write(f"{name},{date_str},{dec_date}\n")
