1b6f1bcdae2289949cf8a6c2234d34d58575c8fb
Sanket Wagle <96889743+sanketwagle@users.noreply.github.com>
2026-05-06 12:54:35 -0500

    Fixed quote issue in find_common_sequences

--- fasta_scripts/find_common_sequences.sh
+++ fasta_scripts/find_common_sequences.sh
@@ -1,24 +1,28 @@
 # Takes in a set of fasta files and truncates them down to the common sequences in them all
 # By default a file i.fasta will be truncated as i.fasta.truncated
 
-tempdir = $(mktemp -d)
+tempdir=$(mktemp -d)
 trap 'rm -rf "$tempdir"; exit' ERR EXIT
-filelist="$@"
+filelist=("$@")
 
 for file in "${filelist[@]}"; do
     echo "Gathering sequences from $file"
-    sed -n "/>/p" $file | sed -e "s/^>//g" > $tempdir/${file}_sequences
+    echo "$tempdir/${file}_sequences"
+    sed -n "/>/p" $file | sed -e "s/^>//g" | sort | uniq > "$tempdir/${file}_sequences"
 done
 
-cat $tempdir/$1_sequences > $tempdir/common_sequences
+cat "$tempdir/$1_sequences" > "$tempdir/common_sequences"
 echo "Finding subset"
 for file in "${filelist[@]}"; do
-    comm -12 $tempdir/common_sequences $file | sort > $tempdir/temporary_file
-    cat $tempdir/temporary_file > $tempdir/common_sequences
+    comm -12 "$tempdir/common_sequences" "$tempdir/${file}_sequences" | sort > "$tempdir/temporary_file"
+    cat "$tempdir/temporary_file" > "$tempdir/common_sequences"
 done
 
-echo "Found $(wc -l $tempdir/common_sequences) common headers"
+echo "Found $(wc -l < $tempdir/common_sequences) common headers"
 
 for file in "${filelist[@]}"; do
-    smof grep -f $tempdir/common_sequences $file > $file.truncated
+    echo "Truncating $file"
+    smof grep -f "$tempdir/common_sequences" $file > $file.truncated
 done
+    
+        
